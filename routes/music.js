d3b3a00bb85150985e17ce6aa27a938ebb579ef8
RyanGallagher44 <rgallag1@stevens.edu>
2023-12-19 19:07:29 -0500

    adding spotify api

--- routes/music.js
+++ routes/music.js
@@ -1,8 +1,31 @@
 import { Router } from 'express';
 const router = Router();
+import querystring from 'node:querystring';
+import axios from 'axios';
 
-router.get('/', (req, res) => {
-    res.json({success: "true"});
+router.get('/', async (req, res) => {
+    const config = {
+        headers: { Authorization: `Bearer ${req.query.code}` }
+    };
+
+    const { data } = await axios.get('https://api.spotify.com/v1/search?q=famous', config);
+
+    res.json(data);
+});
+
+let client_id = '132f4ea63f7c4eb58c7ce744a33479fa';
+let redirect_uri = 'http://localhost:3030/music';
+
+router.get('/login', function(req, res) {
+  let scope = 'user-read-private user-read-email';
+
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri
+    }));
 });
 
 export default router;
