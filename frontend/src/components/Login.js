9fdba1b99f1e9d8f2a28541b0a66827ecb62d7e2
RyanGallagher44 <rgallag1@stevens.edu>
2024-01-02 19:15:28 -0500

    Fixed login issue

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -7,35 +7,34 @@ const Login = () => {
     const [userFollowingData, setUserFollowingData] = useState(undefined);
     const [loading, setLoading] = useState(true);
     const [searchParams, setSearchParams] = useSearchParams();
-    const token = searchParams.get('token');
-    
+    const token = searchParams.get('token') || localStorage.getItem('access_token'); // Get token from URL or local storage
+
     useEffect(() => {
-        localStorage.setItem('access_token', searchParams.get('token'));
-
-        const fetchData = async () => {
-            try {
-                const { data } = await axios.get('https://api.spotify.com/v1/me', {
-                    headers: {
-                        Authorization: 'Bearer ' + token
-                    }
-                });
-                setUserData(data);
-
-                const data2 = await axios.get('https://api.spotify.com/v1/me/following?type=artist', {
-                    headers: {
-                        Authorization: 'Bearer ' + token
-                    }
-                });
-                setUserFollowingData(data2.data.artists.items);
-                console.log(data2.data.artists.items);
-                
-                setLoading(false);
-            } catch (e) {
-                setLoading(false);
-            }
-        };
-
         if (token) {
+            localStorage.setItem('access_token', token);
+
+            const fetchData = async () => {
+                try {
+                    const { data } = await axios.get('https://api.spotify.com/v1/me', {
+                        headers: {
+                            Authorization: 'Bearer ' + token
+                        }
+                    });
+                    setUserData(data);
+
+                    const data2 = await axios.get('https://api.spotify.com/v1/me/following?type=artist', {
+                        headers: {
+                            Authorization: 'Bearer ' + token
+                        }
+                    });
+                    setUserFollowingData(data2.data.artists.items);
+
+                    setLoading(false);
+                } catch (e) {
+                    setLoading(false);
+                }
+            };
+
             fetchData();
         }
     }, [token]);
@@ -44,37 +43,38 @@ const Login = () => {
         window.location.href = 'http://localhost:3030/auth/login';
     };
 
-    if (loading) {
-        return(
-            <div>Loading...</div>
-        );
-    } else {
-        return(
-            <div>
-                {!token &&
-                    <button id="spotify" type="button" onClick={handleClick} class="h-12 w-12 transform m-36 rounded-full border-2 border-green-500 bg-white text-2xl text-green-500 duration-500 hover:-translate-y-3 hover:bg-green-500 hover:text-white">
-                        <i class="fab fa-spotify"></i>
-                    </button>
-                }   
-                {!(!userData) &&
-                    <div>
-                        <h1 class="mt-36">Welcome, {userData.display_name}</h1>
-                        <div class="grid grid-cols-2 md:grid-cols-3 gap-4 p-4">
-                            {userFollowingData.map((artist) => {
-                                return(
-                                    <div>
-                                        <div class="col-span-1 justify-center items-center flex">
-                                            <img class="transform transition duration-500 hover:scale-105 w-72 h-72 object-cover rounded-full" src={artist.images[0].url} alt="" />
-                                        </div>
-                                    </div>
-                                );
-                            })}
-                        </div>
+    return (
+        <div>
+            {!token && (
+                <button
+                    id="spotify"
+                    type="button"
+                    onClick={handleClick}
+                    className="h-12 w-12 transform m-36 rounded-full border-2 border-green-500 bg-white text-2xl text-green-500 duration-500 hover:-translate-y-3 hover:bg-green-500 hover:text-white"
+                >
+                    <i className="fab fa-spotify"></i>
+                </button>
+            )}
+            {token && !loading && (
+                <div>
+                    <h1 className="mt-36">Welcome, {userData.display_name}</h1>
+                    <div className="grid grid-cols-2 md:grid-cols-3 gap-4 p-4">
+                        {userFollowingData.map((artist) => (
+                            <div key={artist.id}>
+                                <div className="col-span-1 justify-center items-center flex">
+                                    <img
+                                        className="transform transition duration-500 hover:scale-105 w-72 h-72 object-cover rounded-full"
+                                        src={artist.images[0].url}
+                                        alt=""
+                                    />
+                                </div>
+                            </div>
+                        ))}
                     </div>
-                }
-            </div>
-        );
-    }
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default Login;
