0e58c6d05258908cc0c37892983dfb24a039f295
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2024-01-04 20:31:58 -0500

    nae nae
    yay

--- frontend/src/components/Artist.js
+++ frontend/src/components/Artist.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import {useEffect} from 'react'
+import {useState} from "react"
+import axios from "axios"
 
 const Artist = () => {
+    const [artist, setArtist] = useState(undefined)
+    const [loading, setLoading] = useState(true)
     const { id } = useParams();
-
-    return(
+    useEffect(() => {
+        async function fetchData() {
+          let {data} = await axios.post(`http://localhost:3030/artist/${id}`, {
+                accessToken:localStorage.getItem("access_token")
+                
+            })
+            setArtist(data)
+            console.log(data)
+            setLoading(false)
+        }
+        fetchData()
+    },[id])
+    if (loading) {
+        return (
+            <div className="m-36">
+                loading
+            </div>
+        );
+    } else {
+        return (
         <div className="m-36">
-            {id}
-        </div>
-    );
+        {id}
+    </div>
+); 
+    }
+    
 
 };
 
--- routes/artists.js
+++ routes/artists.js
@@ -14,4 +14,17 @@ router.post('/search', async (req, res) => {
     res.json(data)
 });
 
+router.post(`/:id`, async (req, res) => {
+    let accessToken = req.body.accessToken
+    let id = req.params.id
+    let {data} = await axios.get(`https://api.spotify.com/v1/artists/${id}/top-tracks`, {
+        headers: {
+        Authorization: 'Bearer' + accessToken
+        }
+    })
+    res.json(data)
+})
+
+
+
 export default router;
