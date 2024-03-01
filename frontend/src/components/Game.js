82122a63f2edcbbeea84f6db96aa3fbfb6ffaff8
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2024-02-29 19:51:43 -0500

    play game track audio

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Loading from "./Loading";
+import ReactPlayer from "react-player";
 
 const Game = () => {
     const [track, setTrack] = useState(undefined)
     const [loading, setLoading] = useState(true)
+    const [playerReady, setPlayerReady] = useState(false);
 
 
 useEffect(() => {
@@ -19,16 +21,33 @@ useEffect(() => {
     fetchData()
 }, [])
 
+const handlePlayerReady = () => {
+    setPlayerReady(true);
+  };
+
 if (loading) {
     return <Loading />
 } else {
     return (
         <div className="mt-32">
             <div>
-            {track.tracks.items[0].name}
+            {track.tracks.items[0].preview_url  && (
+          <ReactPlayer
+            url={track.tracks.items[0].preview_url}
+            playing={true}
+            loop={true}
+            width="0px"
+            height="0px"
+            onReady={handlePlayerReady}
+          />
+        )}
             </div>
             <div>
             {track.tracks.items[0].artists[0].name}
+            </div>
+
+            <div>
+               {track.tracks.items[0].name}
             </div>
 
         </div>
