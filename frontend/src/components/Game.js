9c413561549c35b2ab7840a1aa7a75010441a831
RyanGallagher44 <rgallag1@stevens.edu>
2024-04-11 18:57:12 -0400

    Update Game.js

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -1,58 +1,61 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import Loading from "./Loading";
 import ReactPlayer from "react-player";
 
 const Game = () => {
-    const [track, setTrack] = useState(undefined)
-    const [loading, setLoading] = useState(true)
-    const [playerReady, setPlayerReady] = useState(false);
+  const [track, setTrack] = useState(undefined);
+  const [loading, setLoading] = useState(true);
+  const [playerReady, setPlayerReady] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(true);
+  const [seconds, setSeconds] = useState(1);
+  const playerRef = useRef();
 
-
-useEffect(() => {
+  useEffect(() => {
     async function fetchData() {
-        const { data } = await axios.post("http://localhost:3030/track/search", {
-            accessToken: localStorage.getItem("access_token")
-        })
-        console.log(data)
-        setTrack(data)
-        setLoading(false)
+      const { data } = await axios.post("http://localhost:3030/track/search", {
+        accessToken: localStorage.getItem("access_token"),
+      });
+      console.log(data);
+      setTrack(data);
+      setLoading(false);
     }
-    fetchData()
-}, [])
+    fetchData();
+  }, []);
 
-const handlePlayerReady = () => {
+  const handlePlayerReady = () => {
     setPlayerReady(true);
   };
 
-if (loading) {
-    return <Loading />
-} else {
+  const handleProgress = (progress) => {
+    if (progress.playedSeconds > seconds) {
+      setIsPlaying(false);
+      playerRef.current.seekTo(0);
+    }
+  };
+
+  if (loading) {
+    return <Loading />;
+  } else {
     return (
-        <div className="mt-32">
-            <div>
-            {track.tracks.items[0].preview_url  && (
-          <ReactPlayer
-            url={track.tracks.items[0].preview_url}
-            playing={true}
-            loop={true}
-            width="0px"
-            height="0px"
-            onReady={handlePlayerReady}
-          />
-        )}
-            </div>
-            <div>
-            {track.tracks.items[0].artists[0].name}
-            </div>
-
-            <div>
-               {track.tracks.items[0].name}
-            </div>
-
+      <div className="mt-32 text-center">
+        <div>{track.tracks.items[0].name}</div>
+        <div>{track.tracks.items[0].artists[0].name}</div>
+        <div className="text-center">
+          {track.tracks.items[0].preview_url && (
+            <ReactPlayer
+              ref={playerRef}
+              controls={true}
+              playing={isPlaying}
+              url={track.tracks.items[0].preview_url}
+              onReady={handlePlayerReady}
+              onProgress={handleProgress}
+            />
+          )}
         </div>
-    )
-}
-}
+      </div>
+    );
+  }
+};
 
-export default Game
+export default Game;
